6fa9001cb947a2152ac8a9a5c8b1653df1037721
gustavobarez <gbarez44@gmail.com>
2025-06-04 20:58:50 -0300

    Create product entity

---
+++ src/main/java/com/codefathers/model/entity/Product.java
@@ -0,0 +1,30 @@
+package com.codefathers.model.entity;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "product")
+public class Product {
+    
+    @Id
+    @Column(length = 50)
+    private String sku;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(columnDefinition = "text")
+    private String description;
+
+    @Column(name = "buy_price", precision = 19, scale = 4, nullable = false)
+    private BigDecimal buyPrice;
+
+    @Column(name = "sell_price", precision = 19, scale = 4, nullable = false)
+    private BigDecimal sellPrice;
+
+}
