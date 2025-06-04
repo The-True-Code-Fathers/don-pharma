7a31e64673983ac0747f7f96e6cee813133e4e77
gustavobarez <gbarez44@gmail.com>
2025-06-04 20:18:25 -0300

    Fixes bigdecimal scale on payment entity

--- src/main/java/com/codefathers/model/entity/Payment.java
+++ src/main/java/com/codefathers/model/entity/Payment.java
@@ -4,14 +4,30 @@ import lombok.Data;
 
 import java.math.BigDecimal;
 
+import jakarta.persistence.Column;
+
 @Data
 public class Payment {
     private final int employeeId;
+    
+    @Column(precision = 19, scale = 4)
     private BigDecimal amountInTaxes;
+
+    @Column(precision = 19, scale = 4)
     private BigDecimal grossIncome;
+
+    @Column(precision = 19, scale = 4)
     private BigDecimal mealVoucherAmount;
+
+    @Column(precision = 19, scale = 4)
     private BigDecimal foodVoucherAmount;
+
+    @Column(precision = 19, scale = 4)
     private BigDecimal healthInsuranceAmount;
+
+    @Column(precision = 19, scale = 4)
     private BigDecimal dentalInsuranceAmount;
+
+    @Column(precision = 19, scale = 4)
     private BigDecimal profitSharingAmount;
 }
