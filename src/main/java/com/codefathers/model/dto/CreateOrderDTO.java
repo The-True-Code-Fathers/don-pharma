e7b0b08aef539519cb8bfc0a773cc2a62829823e
Matheus de Bortoli Pastega <matheuspastega@gmail.com>
2025-06-06 10:58:25 -0300

    Add validation to CreateOrderDTO

--- src/main/java/com/codefathers/model/dto/CreateOrderDTO.java
+++ src/main/java/com/codefathers/model/dto/CreateOrderDTO.java
@@ -1,10 +1,15 @@
 package com.codefathers.model.dto;
 
+import com.codefathers.model.entity.Product;
+import com.codefathers.model.entity.ShippingProvider;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -12,7 +17,35 @@ import java.util.UUID;
 @NoArgsConstructor
 @Builder
 public class CreateOrderDTO {
-    // private UUID id;
+
+    // ID will be generated
+    @NotNull
     private UUID sellerId;
+
+    @Size(max = 2048)
     private String description;
+
+    @NotEmpty
+    private List<@Valid CreateOrderItemDTO> item;
+
+    // Products price will be computed
+    @NotNull
+    private ShippingProvider shippingProvider;
+
+    // Shipping price will be computed
+    // Total amount will be computer
+    // Invoiced is false by default
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class CreateOrderItemDTO {
+        @NotNull
+        private Product product;
+
+        @Min(1)
+        private int quantity;
+        // Price will be computed
+    }
 }
--- src/main/java/com/codefathers/service/OrderService.java
+++ src/main/java/com/codefathers/service/OrderService.java
@@ -5,26 +5,42 @@ import com.codefathers.model.entity.Employee;
 import com.codefathers.model.entity.Order;
 import com.codefathers.repository.EmployeeRepository;
 import com.codefathers.repository.OrderRepository;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Valid;
+import jakarta.validation.Validator;
+
+import java.util.UUID;
 
 public class OrderService {
 
-    private OrderRepository orderRepository;
-    private EmployeeRepository employeeRepository;
+    private final OrderRepository orderRepository;
+    private final EmployeeRepository employeeRepository;
+    private final Validator validator;
 
     public OrderService(OrderRepository orderRepository,
-                        EmployeeRepository employeeRepository) {
+                        EmployeeRepository employeeRepository,
+                        Validator validator) {
         this.orderRepository = orderRepository;
         this.employeeRepository = employeeRepository;
+        this.validator = validator;
     }
 
-    public void createOrder(CreateOrderDTO createOrderDTO) {
-        Employee emp = employeeRepository.findById(createOrderDTO.getSellerId());
+    public void createOrder(@Valid CreateOrderDTO createOrderDTO) {
 
+        var violations = validator.validate(createOrderDTO);
+        if (!violations.isEmpty()) {
+            throw new ConstraintViolationException(violations);
+        }
+
+        Employee emp = employeeRepository.findById(createOrderDTO.getSellerId());
         Order order = Order.builder()
                 .seller(emp)
                 .build();
-
         orderRepository.save(order);
+    }
+
+    public Order findOrderById(UUID id) {
+        return new Order();
     }
 
 }
