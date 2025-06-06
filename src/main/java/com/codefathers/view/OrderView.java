bf09cbcb693a499608269a8c227c627abde9bbb6
Matheus de Bortoli Pastega <matheuspastega@gmail.com>
2025-06-06 12:35:23 -0300

    Update OrderView

--- src/main/java/com/codefathers/view/OrderView.java
+++ src/main/java/com/codefathers/view/OrderView.java
@@ -1,10 +1,9 @@
 package com.codefathers.view;
 
-import com.codefathers.model.dto.CreateOrderDTO;
-import com.codefathers.model.entity.Order;
 import com.codefathers.repository.EmployeeRepositoryImpl;
 import com.codefathers.repository.OrderRepositoryImpl;
 import com.codefathers.service.OrderService;
+import com.codefathers.util.ValidationUtil;
 
 public class OrderView {
 
@@ -15,14 +14,16 @@ public class OrderView {
     }
 
     public void render() {
-        var jorge = CreateOrderDTO.builder().build();
-        orderService.createOrder(jorge);
+//        var jorge = CreateOrderDTO.builder().build();
+//        orderService.createOrder(jorge);
+        System.out.println("Hello, World!");
     }
 
     public static void main(String[] args) {
         new OrderView(
                 new OrderService(new OrderRepositoryImpl(),
-                                 new EmployeeRepositoryImpl())
+                                 new EmployeeRepositoryImpl(),
+                                 ValidationUtil.getValidator())
         ).render();
     }
 }
