7d3923f7f98e2f1c8b19f0cb73e62b3e0abfade7
Matheus de Bortoli Pastega <matheuspastega@gmail.com>
2025-06-06 11:57:43 -0300

    Mock EmployeeRepository

--- src/main/java/com/codefathers/repository/EmployeeRepository.java
+++ src/main/java/com/codefathers/repository/EmployeeRepository.java
@@ -5,5 +5,5 @@ import com.codefathers.model.entity.Employee;
 import java.util.UUID;
 
 public interface EmployeeRepository {
-    Employee findById(UUID id);
+    Employee searchEmployeePerId(UUID id);
 }
--- src/main/java/com/codefathers/repository/EmployeeRepositoryImpl.java
+++ src/main/java/com/codefathers/repository/EmployeeRepositoryImpl.java
@@ -1,12 +1,17 @@
 package com.codefathers.repository;
 
 import com.codefathers.model.entity.Employee;
+import com.codefathers.model.enums.EmployeeGender;
+import com.codefathers.model.enums.EmployeeRole;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 public class EmployeeRepositoryImpl implements EmployeeRepository {
+
     @Override
-    public Employee findById(UUID id) {
-        return null;
+    public Employee searchEmployeePerId(UUID id) {
+        return new Employee("Nelson", LocalDate.now(), EmployeeGender.MALE, EmployeeRole.SALES);
     }
 }
+
