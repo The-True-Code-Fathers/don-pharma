0af04990c8225014de678efca29ba0577ad8c86e
Matheus de Bortoli Pastega <matheuspastega@gmail.com>
2025-06-06 09:55:03 -0300

    Define and Implement OrderRepository

--- src/main/java/com/codefathers/repository/OrderRepositoryImpl.java
+++ src/main/java/com/codefathers/repository/OrderRepositoryImpl.java
@@ -2,23 +2,69 @@ package com.codefathers.repository;
 
 import com.codefathers.model.entity.Order;
 import com.codefathers.util.HibernateUtil;
+import org.hibernate.Transaction;
+import org.hibernate.Session;
 
+import java.util.List;
 import java.util.UUID;
 
 public class OrderRepositoryImpl implements OrderRepository {
     @Override
     public void save(Order order) {
-        try (var session = HibernateUtil.getSessionFactory().openSession()) {
-            session.beginTransaction();
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
             session.save(order);
-            session.getTransaction().commit();
+            transaction.commit();
         } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            e.printStackTrace(); // melhor usar logger
+        }
+    }
+
+    @Override
+    public void update(Order order) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.update(order);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void delete(Order order) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.delete(order);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
             e.printStackTrace();
         }
     }
 
     @Override
     public Order findById(UUID id) {
-        return new Order();
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.get(Order.class, id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public List<Order> findAll() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("select Order o from \"order\"", Order.class).list();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return List.of();
+        }
     }
 }
