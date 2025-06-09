9ffc5f729e40791607ffe7a47010e429e72a7686
gustavobarez <gbarez44@gmail.com>
2025-06-09 11:25:05 -0300

    Removes productSku from storage

--- src/main/java/com/codefathers/model/entity/Storage.java
+++ src/main/java/com/codefathers/model/entity/Storage.java
@@ -1,21 +1,29 @@
 package com.codefathers.model.entity;
 
+import java.util.UUID;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 @Entity(name = "storage")
 public class Storage {
     
     @Id
-    @Column(name = "product_sku")
-    private String productSku;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
     @OneToOne
     @JoinColumn(name = "product_sku", referencedColumnName = "sku")
