42fd15d13865ec5274658d8567be23b699478dfd
Matheus de Bortoli Pastega <matheuspastega@gmail.com>
2025-06-09 10:55:09 -0300

    Add vaadin to the project

---
+++ src/main/java/com/codefathers/view/ProductsView.java
@@ -0,0 +1,15 @@
+package com.codefathers.view;
+
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+
+@Route("/products")
+public class ProductsView extends VerticalLayout {
+
+    public ProductsView() {
+        add(new H1("Product List"));
+    }
+
+}
