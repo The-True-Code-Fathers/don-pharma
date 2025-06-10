a701cbde10bb172351e6b5f0d197c729156b0bff
Matheus de Bortoli Pastega <matheuspastega@gmail.com>
2025-06-09 21:25:25 -0300

    Update imports for ProductsView

--- src/main/java/com/codefathers/view/ProductsView.java
+++ src/main/java/com/codefathers/view/ProductsView.java
@@ -5,7 +5,7 @@ import com.codefathers.model.dto.UpdateProductDTO;
 import com.codefathers.model.entity.Product;
 import com.codefathers.repository.ProductRepositoryImpl;
 import com.codefathers.service.ProductService;
-import com.codefathers.util.ValidationUtil;
+import com.codefathers.util.ValidatorUtil;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.notification.Notification;
@@ -38,7 +38,7 @@ public class ProductsView extends VerticalLayout {
         // Instanciar repositório e service manualmente
         var productRepository = new ProductRepositoryImpl();
 
-        this.productService = new ProductService(productRepository, ValidationUtil.getValidator());
+        this.productService = new ProductService(productRepository, ValidatorUtil.getValidator());
 
         setupForm();
         setupGrid();
