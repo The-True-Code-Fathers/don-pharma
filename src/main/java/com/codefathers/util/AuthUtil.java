7bf26191f7b2cec2f2b83cf8087714fc75494a7d
Matheus de Bortoli Pastega <matheuspastega@gmail.com>
2025-06-12 09:45:12 -0300

    Create AuthUtil

--- src/main/java/com/codefathers/util/AuthUtil.java
+++ src/main/java/com/codefathers/util/AuthUtil.java
@@ -1,4 +1,18 @@
 package com.codefathers.util;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 public class AuthUtil {
+    public static class PasswordUtil {
+
+        private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
+        public static String hashPassword(String password) {
+            return encoder.encode(password);
+        }
+
+        public static boolean verifyPassword(String password, String hashedPassword) {
+            return encoder.matches(password, hashedPassword);
+        }
+    }
 }
