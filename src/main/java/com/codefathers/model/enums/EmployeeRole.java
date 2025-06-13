1d87d1d63a2f19124e27e5f4b87b96d3a5a95c9a
gustavobarez <gbarez44@gmail.com>
2025-06-13 15:23:48 -0300

    feat: add a better logic to calculate employee IR

--- src/main/java/com/codefathers/model/enums/EmployeeRole.java
+++ src/main/java/com/codefathers/model/enums/EmployeeRole.java
@@ -1,13 +1,17 @@
 package com.codefathers.model.enums;
 
 import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
+
+import com.codefathers.util.TaxCalculatorUtil;
 
 import lombok.Getter;
 
 public enum EmployeeRole {
     STORAGE(Map.of(
-            EmployeeBenefits.AMOUNT_IN_TAXES, new BigDecimal("2000.00"),
+            EmployeeBenefits.AMOUNT_IN_TAXES, new BigDecimal("00.00"),
             EmployeeBenefits.GROSS_INCOME, new BigDecimal("2000.00"),
             EmployeeBenefits.MEAL_VOUCHER, new BigDecimal("300.00"),
             EmployeeBenefits.FOOD_VOUCHER, new BigDecimal("300.00"),
@@ -15,7 +19,7 @@ public enum EmployeeRole {
             EmployeeBenefits.DENTAL_INSURANCE, new BigDecimal("3000.00"),
             EmployeeBenefits.PROFIT_SHARING, new BigDecimal("500.00"))),
     SHIPPING(Map.of(
-            EmployeeBenefits.AMOUNT_IN_TAXES, new BigDecimal("2500.00"),
+            EmployeeBenefits.AMOUNT_IN_TAXES, new BigDecimal("00.00"),
             EmployeeBenefits.GROSS_INCOME, new BigDecimal("2500.00"),
             EmployeeBenefits.MEAL_VOUCHER, new BigDecimal("350.00"),
             EmployeeBenefits.FOOD_VOUCHER, new BigDecimal("350.00"),
@@ -23,7 +27,7 @@ public enum EmployeeRole {
             EmployeeBenefits.DENTAL_INSURANCE, new BigDecimal("3000.00"),
             EmployeeBenefits.PROFIT_SHARING, new BigDecimal("600.00"))),
     SAC(Map.of(
-            EmployeeBenefits.AMOUNT_IN_TAXES, new BigDecimal("3000.00"),
+            EmployeeBenefits.AMOUNT_IN_TAXES, new BigDecimal("00.00"),
             EmployeeBenefits.GROSS_INCOME, new BigDecimal("3000.00"),
             EmployeeBenefits.MEAL_VOUCHER, new BigDecimal("400.00"),
             EmployeeBenefits.FOOD_VOUCHER, new BigDecimal("400.00"),
@@ -31,7 +35,7 @@ public enum EmployeeRole {
             EmployeeBenefits.DENTAL_INSURANCE, new BigDecimal("3000.00"),
             EmployeeBenefits.PROFIT_SHARING, new BigDecimal("700.00"))),
     HR(Map.of(
-            EmployeeBenefits.AMOUNT_IN_TAXES, new BigDecimal("3500.00"),
+            EmployeeBenefits.AMOUNT_IN_TAXES, new BigDecimal("00.00"),
             EmployeeBenefits.GROSS_INCOME, new BigDecimal("3500.00"),
             EmployeeBenefits.MEAL_VOUCHER, new BigDecimal("450.00"),
             EmployeeBenefits.FOOD_VOUCHER, new BigDecimal("450.00"),
@@ -39,7 +43,7 @@ public enum EmployeeRole {
             EmployeeBenefits.DENTAL_INSURANCE, new BigDecimal("3000.00"),
             EmployeeBenefits.PROFIT_SHARING, new BigDecimal("800.00"))),
     FINANCIAL(Map.of(
-            EmployeeBenefits.AMOUNT_IN_TAXES, new BigDecimal("4000.00"),
+            EmployeeBenefits.AMOUNT_IN_TAXES, new BigDecimal("00.00"),
             EmployeeBenefits.GROSS_INCOME, new BigDecimal("4000.00"),
             EmployeeBenefits.MEAL_VOUCHER, new BigDecimal("500.00"),
             EmployeeBenefits.FOOD_VOUCHER, new BigDecimal("500.00"),
@@ -47,7 +51,7 @@ public enum EmployeeRole {
             EmployeeBenefits.DENTAL_INSURANCE, new BigDecimal("3000.00"),
             EmployeeBenefits.PROFIT_SHARING, new BigDecimal("1000.00"))),
     SALES(Map.of(
-            EmployeeBenefits.AMOUNT_IN_TAXES, new BigDecimal("4500.00"),
+            EmployeeBenefits.AMOUNT_IN_TAXES, new BigDecimal("00.00"),
             EmployeeBenefits.GROSS_INCOME, new BigDecimal("4500.00"),
             EmployeeBenefits.MEAL_VOUCHER, new BigDecimal("600.00"),
             EmployeeBenefits.FOOD_VOUCHER, new BigDecimal("600.00"),
@@ -55,7 +59,7 @@ public enum EmployeeRole {
             EmployeeBenefits.DENTAL_INSURANCE, new BigDecimal("3000.00"),
             EmployeeBenefits.PROFIT_SHARING, new BigDecimal("1200.00"))),
     LOCAL_MANAGER(Map.of(
-            EmployeeBenefits.AMOUNT_IN_TAXES, new BigDecimal("6000.00"),
+            EmployeeBenefits.AMOUNT_IN_TAXES, new BigDecimal("00.00"),
             EmployeeBenefits.GROSS_INCOME, new BigDecimal("6000.00"),
             EmployeeBenefits.MEAL_VOUCHER, new BigDecimal("700.00"),
             EmployeeBenefits.FOOD_VOUCHER, new BigDecimal("700.00"),
@@ -64,9 +68,15 @@ public enum EmployeeRole {
             EmployeeBenefits.PROFIT_SHARING, new BigDecimal("1500.00")));
 
     @Getter
-    private final Map<EmployeeBenefits, BigDecimal> benefits;
+    private final Map<EmployeeBenefits, BigDecimal> BENEFITS;
 
     EmployeeRole(Map<EmployeeBenefits, BigDecimal> benefits) {
-        this.benefits = benefits;
+        BigDecimal grossIncome = benefits.get(EmployeeBenefits.GROSS_INCOME);
+        BigDecimal amountInTaxes = TaxCalculatorUtil.calculateIR(grossIncome);
+
+        Map<EmployeeBenefits, BigDecimal> benefitsWithTax = new HashMap<>(benefits);
+        benefitsWithTax.put(EmployeeBenefits.AMOUNT_IN_TAXES, amountInTaxes);
+
+        this.BENEFITS = Collections.unmodifiableMap(benefitsWithTax);
     }
 }
