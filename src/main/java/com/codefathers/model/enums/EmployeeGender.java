c7f05edc8774037c1b1a805d50aa4e1d40598c9e
Nelson Antunes Junior <nelsoantunes2@gmail.com>
2025-06-13 15:01:59 -0300

    Change employee UI

--- src/main/java/com/codefathers/model/enums/EmployeeGender.java
+++ src/main/java/com/codefathers/model/enums/EmployeeGender.java
@@ -4,5 +4,20 @@ public enum EmployeeGender {
     MALE,
     FEMALE,
     NON_BINARY,
-    UNDECLARED,
+    UNDECLARED;
+
+    public String getLabel() {
+        switch (this) {
+            case MALE:
+                return "Male";
+            case FEMALE:
+                return "Female";
+            case NON_BINARY:
+                return "Non Binary";
+            case UNDECLARED:
+                return "Undeclared";
+            default:
+                return this.name(); // Caso padrão
+        }
+    }
 }
